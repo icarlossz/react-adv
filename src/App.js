fb4436ff551d241da5c278d5ad2b20b638ad1b7f
Carlos Sz <carlosszgreen@gmail.com>
2019-08-23 08:58:50 -0500

    :construction: Initial setup done

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+export default function App () {
+  return <h1>I'm a sexy app.</h1>
+}
+
+render(<App />, document.getElementById('root'))
